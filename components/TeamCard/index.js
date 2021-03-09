bcda04a3f3137e94b51fc6fcbcea97097af31d3f
Ivo Krivzov <ivokrivzov@gmail.com>
2021-03-09 07:53:22 -0300

    day 5

--- components/TeamCard/index.js
+++ components/TeamCard/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import {Card
 } from './styled'
 
+import { Link, BrowserRouter as Router, Route, Switch, useParams } from "react-router-dom";
+
+
 const TeamCard = (props) => {
     const  teamInfos  = props.teams;
     console.log(teamInfos);
@@ -9,15 +12,37 @@ const TeamCard = (props) => {
     return(
         <>
             {teamInfos.map((teamInfo,i)  => 
-            <Card id={i}  style={{backgroundColor: "#"+teamInfo.PrimaryColor}}> 
-                <img
-                    src={teamInfo.WikipediaLogoUrl}
-                />
-                <h2 style={{color: "#"+teamInfo.SecondaryColor}}>{teamInfo.City} {teamInfo.Name}</h2>
+            <Card id={i}  style={{backgroundColor: "#"+teamInfo.PrimaryColor}} > 
+            <Router>
+                {/* <Link to={`/team/${teamInfo.Key}`}> */}
+                <Link to={`/${teamInfo.Key}`}>
+
+                    <img
+                        src={teamInfo.WikipediaLogoUrl}
+                    />
+                    <h2 style={{color: "#"+teamInfo.SecondaryColor}}>{teamInfo.City} {teamInfo.Name}</h2>
+                    </Link>
+        <Switch>
+          <Route path="/:id"  children={<Child />}/>
+        </Switch>
+            </Router>
             </Card>
             )}
+           
         </>
     )
 }
 
 export default TeamCard
+
+function Child() {
+    // We can use the `useParams` hook here to access
+    // the dynamic pieces of the URL.
+    let { id } = useParams();
+  
+    return (
+      <div>
+        <h3>ID: {id}</h3>
+      </div>
+    );
+  }
