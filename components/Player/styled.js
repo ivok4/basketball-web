80e5e3d14fad983c27d1c6d8b3e7657c1df8c5dc
Ivo Krivzov <ivokrivzov@gmail.com>
2021-03-16 17:26:24 -0300

    responsive

--- components/Player/styled.js
+++ components/Player/styled.js
@@ -15,36 +15,57 @@ height:70%;
 margin-left:3%;
 p{
         margin:0 3%;
-        font-size:30px;
+        font-size:20px;
         font-family: 'Oswald';
         color:#b8b9c2;
     }
     h1{
         margin:3%;
-        font-size:100px;
+        font-size:50px;
         font-family: 'Oswald Bold';
     }
     div{
         width:100%;
-        height:30%;
+        height:100%;
         display:grid;
         grid-template-columns: 35% 75%;
         margin:0 3%;
-
+        align-items:center;
+        justify-content:center;
         p{
-            width:45%;
+            width:90%;
             float:left;
-            font-size:20px;
+            font-size:15px;
         }
         h2{
             color:#FFF;
-            width:45%; 
-            font-size:20px;
+            width:90%; 
+            font-size:15px;
         }
     }
 }
 
 img{
     width:30%;
+}
+@media (min-width: 768px) {
+.playerInfo{
+p{
+        font-size:30px;
+    }
+    h1{
+        font-size:100px;
+    }
+    div{
+        p{
+            width:45%;
+            font-size:20px;
+        }
+        h2{
+            width:45%; 
+            font-size:20px;
+            }
+        }
+    }
 }
 `
--- containers/Team/styled.js
+++ containers/Team/styled.js
@@ -12,11 +12,17 @@ display:flex;
 flex-direction:column;
 justify-content:space-between;
 align-items:center;
-width:5%;
+width:15%;
 height:100%;
 img{
     width:60%;
     height:5.7%;
     border-radius:50%;
+}
+@media (min-width: 768px) {
+    width:10%;
+}
+@media (min-width: 1024px) {
+    width:5%;
 }
 `
